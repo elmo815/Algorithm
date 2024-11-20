bf11436acef1a867ca30ee3136f26fe990a686be
elmo815 <obliviate815@gmail.com>
2024-11-20 09:50:42 +0900

    [Silver V] Title: 단어 나누기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,12 +1,10 @@
-word = input()
- 
-ans = "z"*51
-for i in range(1, len(word)-1):
-    for j in range(i+1, len(word)):
-        a = word[:i]
-        b = word[i:j]
-        c = word[j:]
-        new_word = a[::-1]+b[::-1]+c[::-1]
-        if new_word < ans:
-            ans = new_word
-print(ans)
+W = input()
+result = []
+
+for i in range(1, len(W)):
+    for j in range(i + 1, len(W)):
+        case1 = W[:i][::-1]
+        case2 = W[i:j][::-1]
+        case3 = W[j:][::-1]
+        result.append(case1 + case2 + case3)
+print(sorted(result)[0])
